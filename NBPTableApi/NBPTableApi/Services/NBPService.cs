41d457d77c9d5fca762b32a7b8af60e0534de41f
WojRcH <wojrch@gmail.com>
2025-10-21 12:28:26 +0200

    HttpClient inject dependency to NBPService

--- NBPTableApi/NBPTableApi/Services/NBPService.cs
+++ NBPTableApi/NBPTableApi/Services/NBPService.cs
@@ -1,21 +1,23 @@
 ﻿using NBPTableApi.AppDbContext;
+using NBPTableApi.Models;
+using System.Net.Http;
 
 namespace NBPTableApi.Services
 {
     public class NBPService : INBPService
     {
-        AppDbContextSqlite dbContext;
-        public NBPService(AppDbContextSqlite dbContext) 
+        private readonly AppDbContextSqlite dbContext;
+        private readonly HttpClient httpClient;
+
+        public NBPService(AppDbContextSqlite dbContext, HttpClient httpClient) 
         {
             this.dbContext = new AppDbContextSqlite();
-
+            this.httpClient = httpClient;
         }
+
         public string GetNBPTable()
         {
-            //dbContext.
             return "";
-
-
         }
     }
 }
