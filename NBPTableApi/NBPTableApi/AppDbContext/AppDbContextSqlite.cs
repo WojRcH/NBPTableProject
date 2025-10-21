5771c938f19aa297d9e85d9a89dc4ffc703a1b4c
WojRcH <wojrch@gmail.com>
2025-10-21 12:52:07 +0200

    Cleanup

--- NBPTableApi/NBPTableApi/AppDbContext/AppDbContextSqlite.cs
+++ NBPTableApi/NBPTableApi/AppDbContext/AppDbContextSqlite.cs
@@ -1,18 +1,13 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using NBPTableApi.Models;
+
 namespace NBPTableApi.AppDbContext
 {   
     public class AppDbContextSqlite : DbContext
     {
-        public AppDbContextSqlite(DbContextOptions<AppDbContextSqlite> options)
-        : base(options)
-        {
-        }
+        public AppDbContextSqlite(DbContextOptions<AppDbContextSqlite> options) : base(options){}
 
         public DbSet<ExchangeRatesTable> ExchangeRatesTableItems { get; set; }
         public DbSet<ExchangeRate> ExchangeRateItems { get; set; }
-
-        
     }
-
 }
